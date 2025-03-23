9fab1183a2198f5b0f309e95eedcc5a2c5ef7c14
65ra8e <willardth77@gmail.com>
2025-03-22 23:51:21 -0700

    first commit

---
+++ pract.py
@@ -0,0 +1,46 @@
+# string concatenation(string glue)
+
+    #print ("20 days are " + str(24 * 60 * 20) + " minutes")  or print(f'20 days are {35 * to_units} seconds')
+    #20 days are 28800 minutes
+ 
+
+
+# "varibles"
+    # name_of_unit = 'seconds'
+
+    # to_units = (24 * 60 * 60)
+    # print(f'20 days are {35 * to_units} {name_of_unit}')
+
+# functions
+
+# name_of_unit = 'seconds'
+
+# to_units = (24 * 60 * 60)
+
+# def days_to_units(num_of_days):
+#     print(f'{num_of_days} days are {num_of_days * to_units} {name_of_unit}')
+#     print("all good")
+
+
+# days_to_units(35)
+# days_to_units(45)
+# days_to_units(100)
+# days_to_units(200)
+
+
+
+
+
+# name_of_unit = 'seconds'
+
+# to_units = (24 * 60 * 60)
+
+# def days_to_units(num_of_days, custom_message):
+#     print(f'{num_of_days} days are {num_of_days * to_units} {name_of_unit}')
+#     print(custom_message)
+
+
+# days_to_units(35, "what's up!")
+# days_to_units(45, "great job")
+# days_to_units(100, "wow, that's a lot!")
+# days_to_units(200, "now, your doing too much")
